8c08489958071e60f9a58d86712646e296e3a98e
Andy Orahoske <orahoske.2@gmail.com>
2018-06-03 12:18:04 -0400

    Add in start script, pre-cider

---
+++ start.sh
@@ -0,0 +1,9 @@
+ln -s ./.cider ~/.cider
+
+python get-pip.py
+
+pip install -U cider
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+cider restore
