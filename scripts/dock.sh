1fccf11a25dbd4cc4379528c3225dafd0b8fca1e
Andy Orahoske <orahoske.2@gmail.com>
2018-06-03 15:09:50 -0400

    Start putting some apps in the dock

---
+++ scripts/dock.sh
@@ -0,0 +1,6 @@
+dockutil --remove all
+
+dockutil --add /Applications/1Password\ 7.app
+dockutil --add /Applications/iTerm.app
+dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/Messages.app
