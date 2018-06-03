7aa20fedd04f2e8595d6a61eb4ab01a972a0b2d9
Andy Orahoske <orahoske.2@gmail.com>
2018-06-02 22:12:27 -0400

    Giving this cider thing a try
    Tweak .gitconfig
    
    Ciderify .gitconfig
    
    Remove all the things, starting from scratch

--- .Caskfile.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-brew install caskroom/cask/brew-cask
-
-brew cask install firefox
-brew cask install google-chrome
-brew cask install mailbox
-brew cask install robomongo
-brew cask install slate
